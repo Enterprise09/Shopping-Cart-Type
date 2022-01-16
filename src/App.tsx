56a58ec8b97f8b6b512cb3a581da60bd8b251450
Enterprise09 <goldenstaytes@gmail.com>
2022-01-16 20:40:02 +0900

    [+] fetch data from fakestore api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import { LinearProgress } from "@material-ui/core";
+import { useQuery } from "react-query";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React to Typescript</h1>
-    </div>
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
-}
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Somthing went wrong ... </div>;
+
+  return <div className="App">Start Hello React to Typescript</div>;
+};
 
 export default App;
